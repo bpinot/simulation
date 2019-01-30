da0755d66453339bc1e4f69c91fe460d1773f44a
bpinot <47177165+bpinot@users.noreply.github.com>
2019-01-30 12:01:29 +0100

    Update Panel.java

--- Nostromo_enum/src/cargo/Panel.java
+++ Nostromo_enum/src/cargo/Panel.java
@@ -17,15 +17,16 @@ public class Panel {
 	}
 
 	public Panel(Mission mission){
+		double masse_batteries = 7; //kg
 		double rendement = 0.25;
 		double transfert = 0.8;
-		double puissance_Terre = 1360.8;
+		double puissance_Terre = 1368;
 		double distance_Terre = 149.6*Math.pow(10,9);	 //distance en m
 		double rapport_masse_surface = 4; // kg/m^2
 		double distance_max_soleil = mission.target.getOrbit().getSemi_major_axis()*(1+mission.target.getOrbit().getEccentricity()); //m
 		
 		this.surface = mission.power/(rendement*transfert*puissance_Terre*Math.pow(distance_Terre/distance_max_soleil,2));
-		this.mass = surface*rapport_masse_surface;
+		this.mass = surface*rapport_masse_surface + masse_batteries;
 //		mission.nostromo.getMass().setPanelMass(this.mass);
 	}
 	
